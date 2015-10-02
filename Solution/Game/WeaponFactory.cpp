bbd66146d8e4df9102eba3fa31e685d4db23bd9d
LastVision <daniel.carlsson92@gmail.com>
2015-10-02 13:09:54 +0200

    Added Menu & Buttons in Menu Filter also added a empty interface for weapon factory

--- Solution/Game/WeaponFactory.cpp
+++ Solution/Game/WeaponFactory.cpp
@@ -10,3 +10,15 @@ WeaponFactory::WeaponFactory()
 WeaponFactory::~WeaponFactory()
 {
 }
+
+
+void WeaponFactory::LoadWeapons(const std::string& aRootFilePath)
+{
+
+}
+
+WeaponData WeaponFactory::GetWeapon(const std::string& aWeaponName)
+{
+	auto it = myWeaponsTypes.find(aWeaponName);
+	return it->second;
+}
--- Solution/Game/WeaponFactory.h
+++ Solution/Game/WeaponFactory.h
@@ -1,8 +1,26 @@
 #pragma once
+#include "Enums.h"
+
+struct WeaponData
+{
+	eBulletType myBulletType;
+	float myCoolDownTime;
+	float myCurrentTime;
+	int mySpread;
+	int myID;
+	CU::Vector3<float> myPosition;
+};
+
 class WeaponFactory
 {
 public:
 	WeaponFactory();
 	~WeaponFactory();
+
+	void LoadWeapons(const std::string& aRootFilePath);
+
+	WeaponData GetWeapon(const std::string& aWeaponName);
+private:
+	std::unordered_map<std::string, WeaponData> myWeaponsTypes;
 };
 
