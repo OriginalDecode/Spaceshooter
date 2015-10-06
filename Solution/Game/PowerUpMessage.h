ad5156eadf824e08787e7405be991238e9242265
D3c0de <ls12_ac-web@hotmail.com>
2015-10-06 15:07:01 +0200

    Preparing for pull

---
+++ Solution/Game/PowerUpMessage.h
@@ -0,0 +1,68 @@
+#pragma once
+
+enum class ePowerUpType
+{
+	FIRERATEBOOST,
+	SHIELDBOOST,
+	HEALTHKIT_01,
+	HEALTHKIT_02,
+
+};
+
+class PowerUpMessage
+{
+public:
+
+	PowerUpMessage(ePowerUpType someType, float someDuration, int someShieldStrength
+		, int someHealthRecover, int someFireRateMultiplier);
+
+	const ePowerUpType GetType();
+	const int GetShieldStrength();
+	const int GetFireRateMultiplier();
+	const int GetHealthRecover();
+	const float GetDuration();
+
+private:
+
+	ePowerUpType myType;
+	float myDuration;
+	int myShieldStrength;
+	int myHealthRecover;
+	int myFireRateMultiplier;
+
+};
+
+inline PowerUpMessage::PowerUpMessage(ePowerUpType someType, float someDuration, int someShieldStrength
+		, int someHealthRecover, int someFireRateMultiplier)
+	: myType(someType)
+	, myDuration(someDuration)
+	, myShieldStrength(someShieldStrength)
+	, myHealthRecover(someHealthRecover)
+	, myFireRateMultiplier(myFireRateMultiplier)
+{
+}
+
+inline const ePowerUpType  PowerUpMessage::GetType()
+{
+	return myType;
+}
+
+inline const int  PowerUpMessage::GetShieldStrength()
+{
+	return myShieldStrength;
+}
+
+inline const int  PowerUpMessage::GetFireRateMultiplier()
+{
+	return myFireRateMultiplier;
+}
+
+inline const int  PowerUpMessage::GetHealthRecover()
+{
+	return myHealthRecover;
+}
+
+inline const float  PowerUpMessage::GetDuration()
+{
+	return myDuration;
+}
