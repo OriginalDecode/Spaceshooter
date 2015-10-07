ff875b29211e6a6c18d8276ac8810de3408b89c4
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-10-07 10:04:58 +0200

    added mission-classes and filter

---
+++ Solution/Game/WaypointMission.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "WaypointMission.h"
+
+
+WaypointMission::WaypointMission()
+{
+}
+
+
+WaypointMission::~WaypointMission()
+{
+}
---
+++ Solution/Game/WaypointMission.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Mission.h"
+class WaypointMission :	public Mission
+{
+public:
+	WaypointMission();
+	~WaypointMission();
+};
+
