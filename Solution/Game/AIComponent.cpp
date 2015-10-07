231d9ddbf29b50e54029d76157bd5180b913b2ff
linus.ljungman@gmail.com <linus.ljungman@gmail.com>
2015-10-07 09:45:49 +0200

    cleaned up AI component.

--- Solution/Game/AIComponent.cpp
+++ Solution/Game/AIComponent.cpp
@@ -16,46 +16,31 @@ void AIComponent::Init()
 {
 	myEntityToFollow = nullptr;
 
-	myTimeToNextDecision = 1.f;
+	myTimeToNextDecision = myTimeBetweenDecisions;
 	mySpeed = 80.f;
 	myVelocity = myEntity.myOrientation.GetForward() * mySpeed;
 }
 
 void AIComponent::Update(float aDeltaTime)
 {
-	if (myEntityToFollow == nullptr)
-	{
-		FollowOwnDecision(aDeltaTime);
-	}
-	else
-	{
-		myTimeToNextDecision -= aDeltaTime;
-		FollowEntity(aDeltaTime);
+	DL_ASSERT_EXP(myEntityToFollow != nullptr, "AI needs an entity to follow.");
 
-		CU::Vector3<float> toTarget = myEntityToFollow->myOrientation.GetPos() - myEntity.myOrientation.GetPos();
+	myTimeToNextDecision -= aDeltaTime;
+	FollowEntity(aDeltaTime);
+
+	CU::Vector3<float> toTarget = myEntityToFollow->myOrientation.GetPos() - myEntity.myOrientation.GetPos();
 		
-		CU::Normalize(toTarget);
-		if (CU::Dot(myEntity.myOrientation.GetForward(), toTarget) > 0.72f && myTimeToNextDecision < 0)
-		{
-			myTimeToNextDecision = 3.f;
-			Shoot();
-		}
+	CU::Normalize(toTarget);
+	if (CU::Dot(myEntity.myOrientation.GetForward(), toTarget) > 0.72f && myTimeToNextDecision < 0)
+	{
+		myTimeToNextDecision = myTimeBetweenDecisions;
+		Shoot();
 	}
 }
 
 void AIComponent::SetEntityToFollow(Entity* aEntity)
 {
 	myEntityToFollow = aEntity;
-}
-
-void AIComponent::MakeDecision()
-{
-	myDecision = rand() % 9;
-	myTimeToNextDecision = 1.f;
-	if (myDecision == 2 || myDecision == 8)
-	{
-		Shoot();
-	}
 }
 
 void AIComponent::FollowEntity(float aDeltaTime)
@@ -98,45 +83,4 @@ void AIComponent::FollowEntity(float aDeltaTime)
 	myVelocity *= mySpeed;
 
 	myEntity.myOrientation.SetPos(myEntity.myOrientation.GetPos() + aDeltaTime * myVelocity);
-}
-
-void AIComponent::FollowOwnDecision(float aDeltaTime)
-{
-	myTimeToNextDecision -= aDeltaTime;
-	if (myTimeToNextDecision <= 0.f)
-	{
-		MakeDecision();
-	}
-
-		
-	switch (myDecision)
-	{
-	case 0:
-		MoveLeft(10.f * aDeltaTime);
-		break;
-	case 1:
-		MoveRight(10.f * aDeltaTime);
-		break;
-	case 2:
-		MoveUp(10.f * aDeltaTime);
-		break;
-	case 3:
-		MoveDown(10.f * aDeltaTime);
-		break;
-	case 4:
-		MoveForward(10.f * aDeltaTime);
-		break;
-	case 5:
-		MoveBackward(10.f * aDeltaTime);
-		break;
-	case 6:
-		RotateX(globalPi / 4.f * aDeltaTime);
-		break;
-	case 7:
-		RotateY(globalPi / 4.f * aDeltaTime);
-		break;
-	case 8:
-		RotateZ(globalPi / 4.f * aDeltaTime);
-		break;
-	}
 }
