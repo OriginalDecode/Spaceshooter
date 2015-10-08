3b0f9f3c01310a651064f86b82909b603ec55892
linus.ljungman@gmail.com <linus.ljungman@gmail.com>
2015-10-08 13:42:24 +0200

    added frustum class

--- Solution/Engine/Octree.cpp
+++ Solution/Engine/Octree.cpp
@@ -1,12 +1,36 @@
 #include "stdafx.h"
+
 #include "Octree.h"
+#include <sstream>
+#include "TreeNode.h"
+#include <Vector.h>
 
-
-Octree::Octree()
+Prism::Octree::Octree(int aMaxDepth)
+	: myMaxDepth(aMaxDepth)
 {
+	myRoot = new TreeNode(CU::Vector3<float>(0, 0, 0), 400.f, nullptr, 0, aMaxDepth);
 }
 
-
-Octree::~Octree()
+Prism::Octree::~Octree()
 {
+	delete myRoot;
+}
+
+void Prism::Octree::Add(Instance* aInstance)
+{
+	myRoot->InsertObjectDown(aInstance);
+}
+
+void Prism::Octree::Update()
+{
+	//std::stringstream ss;
+	//static int numberOfNodes[7];
+	//ss << 
+	//Engine::GetInstance()->PrintDebugText()
+}
+
+void Prism::Octree::GetOccupantsInAABB(const CommonUtilities::Intersection::AABB& aAABB
+		, CU::GrowingArray<Instance*>& aOutArray)
+{
+	myRoot->GetOccupantsInAABB(aAABB, aOutArray);
 }
