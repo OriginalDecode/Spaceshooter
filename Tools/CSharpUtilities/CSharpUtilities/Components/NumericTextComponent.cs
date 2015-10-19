9386d3e275e7518395965835a535e30cc3723ea4
LastVision <daniel.carlsson92@gmail.com>
2015-10-19 15:48:08 +0200

    Entity Edtior - Added Empty implementation of NumericText and Vector3 Components

---
+++ Tools/CSharpUtilities/CSharpUtilities/Components/NumericTextComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CSharpUtilities.Components
+{
+    public class NumericTextComponent : BaseComponent
+    {
+
+        public NumericTextComponent(Point aLocation, Size aSize, string aText, bool aDigitOnlyFlag = true)
+            : base(aLocation, aSize, aText)
+        {
+
+        }
+
+        protected override void InitializeComponents(string aText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BindToPanel(Panel aPanel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Hide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TextBox GetTextBox()
+        {
+            throw new NotImplementedException();
+            return new TextBox();
+        }
+    }
+}
---
+++ Tools/CSharpUtilities/CSharpUtilities/Components/Vector3Component.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CSharpUtilities.Components
+{
+    public class Vector3Component : BaseComponent
+    {
+
+        public Vector3Component(Point aLocation, Size aSize, string aText)
+            : base(aLocation, aSize, aText)
+        {
+
+        }
+
+        protected override void InitializeComponents(string aText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BindToPanel(Panel aPanel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Hide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetX()
+        {
+            throw new NotImplementedException();
+            return 0;
+        }
+
+        public TextBox GetXTextBox()
+        {
+            throw new NotImplementedException();
+            return new TextBox();
+        }
+
+        public float GetY()
+        {
+            throw new NotImplementedException();
+            return 0;
+        }
+
+        public TextBox GetYTextBox()
+        {
+            throw new NotImplementedException();
+            return new TextBox();
+        }
+
+        public float GetZ()
+        {
+            throw new NotImplementedException();
+            return 0;
+        }
+
+        public TextBox GetZTextBox()
+        {
+            throw new NotImplementedException();
+            return new TextBox();
+        }
+
+        public void SetPosition(string aX, string aY, string aZ)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
