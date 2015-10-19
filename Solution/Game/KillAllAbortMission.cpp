fe3bcbde6d37c39a323b66b9945a4ffbe4d49891
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-10-19 14:46:06 +0200

    fixed bugged missions

--- Solution/Game/KillAllAbortMission.cpp
+++ Solution/Game/KillAllAbortMission.cpp
@@ -20,5 +20,5 @@ bool KillAllAbortMission::Update(float aDeltaTime)
 	CU::Vector2<float> screenCenter(engine->GetWindowSize().x * 0.5f, engine->GetWindowSize().y * 0.5f);
 	engine->PrintDebugText("Current mission: Kill all (abort) enemies!", { screenCenter.x - 300, -(screenCenter.y) + screenCenter.y * 0.5f });
 
-	return myLevel.GetEnemiesAlive() == myAbortEnemiesLeft;
+	return myLevel.GetEnemiesAlive() <= myAbortEnemiesLeft;
 }
