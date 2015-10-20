6ef73b0ff2630658c38a9cd5c66c9ca82cbc9f57
linus.ljungman.sp14@thegameassembly.com <linus.ljungman.sp14@thegameassembly.com>
2015-10-20 15:24:38 +0200

    started working on mission container (but commented out for work on Niklas PC)

--- Solution/Game/MissionContainer.cpp
+++ Solution/Game/MissionContainer.cpp
@@ -1,12 +1,41 @@
 #include "stdafx.h"
-#include "MissionContainer.h"
-
-
-MissionContainer::MissionContainer()
-{
-}
-
-
-MissionContainer::~MissionContainer()
-{
-}
+//#include <sstream>
+//#include "MissionContainer.h"
+//#include "XMLReader.h"
+//
+//
+//MissionContainer::MissionContainer(XMLReader& aReader, tinyxml2::XMLElement* aElement)
+//	: Mission(aReader, aElement)
+//{
+//	tinyxml2::XMLElement* element = aReader.ForceFindFirstChild(aElement, "secondsvisual");
+//	aReader.ForceReadAttribute(element, "value", myVisualTimeStart);
+//	element = aReader.ForceFindFirstChild(aElement, "secondsreal");
+//	aReader.ForceReadAttribute(element, "value", myRealTimeStart);
+//}
+//
+//bool MissionContainer::Update(float aDeltaTime)
+//{
+//	std::stringstream ss;
+//	ss.precision(2);
+//	ss << "Current mission: Survive for: " << myVisualTime << " seconds (abort)";
+//
+//	Prism::Engine* engine = Prism::Engine::GetInstance();
+//	CU::Vector2<float> screenCenter(engine->GetWindowSize().x * 0.5f, engine->GetWindowSize().y * 0.5f);
+//
+//	engine->PrintDebugText(ss.str(), { screenCenter.x - 300, -(screenCenter.y) + screenCenter.y * 0.5f });
+//	myVisualTime -= aDeltaTime;
+//	myRealTime -= aDeltaTime;
+//	return myRealTime <= 0;
+//}
+//
+//void MissionContainer::Start()
+//{
+//	myVisualTime = myVisualTimeStart;
+//	myRealTime = myRealTimeStart;
+//}
+//
+//void MissionContainer::End()
+//{
+//	myVisualTime = 0.f;
+//	myRealTime = 0.f;
+//}
--- Solution/Game/MissionContainer.h
+++ Solution/Game/MissionContainer.h
@@ -1,8 +1,24 @@
-#pragma once
-class MissionContainer
-{
-public:
-	MissionContainer();
-	~MissionContainer();
-};
-
+//#pragma once
+//#include <GrowingArray.h>
+//#include "Mission.h"
+//
+//class XMLReader;
+//namespace tinyxml2
+//{
+//	class XMLElement;
+//}
+//
+//class MissionContainer : public Mission
+//{
+//public:
+//	MissionContainer(XMLReader& aReader, tinyxml2::XMLElement* aElement);
+//
+//	bool Update(float aDeltaTime) override;
+//	void Start() override;
+//	void End() override;
+//
+//private:
+//	CU::GrowingArray<Mission*> myRequiredMissions;
+//	CU::GrowingArray<Mission*> myOptionalMissions;
+//};
+//
