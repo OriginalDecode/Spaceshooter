4bf2fed296a9023acdc0afb388718a5a8e96374b
LastVision <daniel.carlsson92@gmail.com>
2015-11-02 10:56:29 +0100

    ParticleEditor - Added DLL Window

--- Tools/ParticleEditor/ParticleEditor/ParticleEditor.Designer.cs
+++ Tools/ParticleEditor/ParticleEditor/ParticleEditor.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.myParticleWindow = new System.Windows.Forms.Panel();
+            this.UpdateTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // myParticleWindow
+            // 
+            this.myParticleWindow.Location = new System.Drawing.Point(14, 13);
+            this.myParticleWindow.Name = "myParticleWindow";
+            this.myParticleWindow.Size = new System.Drawing.Size(450, 467);
+            this.myParticleWindow.TabIndex = 0;
+            // 
+            // UpdateTimer
+            // 
+            this.UpdateTimer.Interval = 1;
+            this.UpdateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
             // 
             // ParticleEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 511);
+            this.Controls.Add(this.myParticleWindow);
             this.Name = "ParticleEditor";
             this.Text = "Particle Editor";
             this.ResumeLayout(false);
@@ -42,6 +58,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel myParticleWindow;
+        private System.Windows.Forms.Timer UpdateTimer;
     }
 }
 
