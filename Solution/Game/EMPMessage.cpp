90918a9351935b82a73d1554bb42636f2ba56c4b
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-11-02 16:19:49 +0100

    new message

---
+++ Solution/Game/EMPMessage.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "EMPMessage.h"
+
+
+EMPMessage::EMPMessage()
+{
+}
+
+
+EMPMessage::~EMPMessage()
+{
+}
---
+++ Solution/Game/EMPMessage.h
@@ -0,0 +1,8 @@
+#pragma once
+class EMPMessage
+{
+public:
+	EMPMessage();
+	~EMPMessage();
+};
+
