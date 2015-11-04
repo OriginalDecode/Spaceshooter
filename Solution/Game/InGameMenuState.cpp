f362c2ddf7b02a3d955216dc4283013ac1bd0389
fabs <fabian.ahlund.sp14@thegameassembly.com>
2015-11-04 15:57:37 +0100

    added class

---
+++ Solution/Game/InGameMenuState.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "InGameMenuState.h"
+
+
+InGameMenuState::InGameMenuState()
+{
+}
+
+
+InGameMenuState::~InGameMenuState()
+{
+}
---
+++ Solution/Game/InGameMenuState.h
@@ -0,0 +1,8 @@
+#pragma once
+class InGameMenuState
+{
+public:
+	InGameMenuState();
+	~InGameMenuState();
+};
+
