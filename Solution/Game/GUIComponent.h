8a43ec5358453e21275520f563a0c8b21232f407
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2015-11-05 14:06:21 +0100

    Beautiful fade in when locking to target

--- Solution/Game/GUIComponent.h
+++ Solution/Game/GUIComponent.h
@@ -25,7 +25,7 @@ public:
 	void Update(float aDeltaTime) override;
 	void CalculateAndRender(const CU::Vector3<float>& aPosition, Prism::Sprite* aCurrentModel
 		, Prism::Sprite* aArrowModel, Prism::Sprite* aMarkerModel, const CU::Vector2<int> aWindowSize
-		, bool aShowDist, bool aIsPowerup = false, std::string aName = "");
+		, bool aShowDist, float anAlpha = 1.f, bool aIsPowerup = false, std::string aName = "");
 
 	void Render(const CU::Vector2<int> aWindowSize, const CU::Vector2<float> aMousePos);
 
