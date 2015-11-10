49934e03607baa0d5282f02d4d4b9730008cf5b9
nian0601 <anderssonniklas@outlook.com>
2015-11-10 10:22:22 +0100

    Fixed cursor hiding and locking

--- Solution/Game/Game.cpp
+++ Solution/Game/Game.cpp
@@ -215,6 +215,7 @@ void Game::ReceiveMessage(const GameStateMessage& aMessage)
 		break;
 	case eGameState::MOUSE_LOCK:
 		myLockMouse = aMessage.GetMouseLocked();
+		ShowCursor(!myLockMouse);
 		break;
 	case eGameState::COMPLETE_GAME:
 		myIsComplete = true;
