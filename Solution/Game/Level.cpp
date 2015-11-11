2d5a69e696635214b91ca596c2d6cd4d91dd3eaa
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2015-11-11 18:11:33 +0100

    Moved text

--- Solution/Game/Level.cpp
+++ Solution/Game/Level.cpp
@@ -284,7 +284,7 @@ void Level::Render()
 	if (myIsSkipable == true)
 	{
 		Prism::Engine::GetInstance()->PrintText("Press [Enter] to skip tutorial."
-			, { (Prism::Engine::GetInstance()->GetWindowSize().y * 0.5f) * 1.5f, -(Prism::Engine::GetInstance()->GetWindowSize().y * 0.5f) * 1.6f }
+			, { (Prism::Engine::GetInstance()->GetWindowSize().y * 0.5f) * 1.45f, -(Prism::Engine::GetInstance()->GetWindowSize().y * 0.5f) * 1.55f }
 			, Prism::eTextType::RELEASE_TEXT);
 	}
 	
