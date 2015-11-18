1d40932d9c437315fb5ce5df0cba112c0e6e0511
nian0601 <anderssonniklas@outlook.com>
2015-11-18 13:54:18 +0100

    Added FancyFade to EMP-effect

--- Solution/Game/InputComponent.cpp
+++ Solution/Game/InputComponent.cpp
@@ -18,6 +18,9 @@
 #include <sstream>
 #include <XMLReader.h>
 
+#include "PostMaster.h"
+#include "EMPMessage.h"
+
 InputComponent::InputComponent(Entity& aEntity)
 	: ControllerComponent(aEntity)
 {
@@ -68,6 +71,7 @@ void InputComponent::Update(float aDeltaTime)
 		if (myInputWrapper->KeyIsPressed(DIK_SPACE))
 		{
 			myEntity.SendNote(EMPNote());
+			PostMaster::GetInstance()->SendMessage(EMPMessage(myEntity.myOrientation.GetPos(), 10.f));
 		}
 
 		if (myInputWrapper->MouseIsPressed(0) == true)
